43a2a124e830172c9b82b1ec219b2630036f6a18
Amr Reda <amr.redab1398@gmail.com>
2024-11-05 19:55:11 +0200

    added loading CircularProgress ui to the home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Box, Container } from "@mui/material";
+import { CircularProgress, Box, Container } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import ProductCard from "../components/ProductCard";
 import { useEffect, useState } from "react";
@@ -9,6 +9,8 @@ const HomePage = () => {
   const [products, setProdcuts] = useState<productCardProps[]>([]);
   //handel errors
   const [error, setError] = useState(false);
+  //loading state
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     //modern syntax to fetch data from a server (more readable , scalability(no nesting then() (callback hell) ))
@@ -19,6 +21,8 @@ const HomePage = () => {
         setProdcuts(data);
       } catch {
         setError(true);
+      } finally {
+        setLoading(false);
       }
     };
     fetchData();
@@ -33,14 +37,25 @@ const HomePage = () => {
   }
 
   return (
-    <Container sx={{ mt: 2 }}>
-      <Grid container spacing={3}>
-        {products.map((p: productCardProps) => (
-          <Grid key={p._id} size={{ xs: 12, sm: 6, md: 4 }}>
-            <ProductCard {...p}></ProductCard>
-          </Grid>
-        ))}
-      </Grid>
+    <Container sx={{ mt: 5 }}>
+      {loading ? (
+        <Grid
+          container
+          justifyContent="center"
+          alignItems="center"
+          style={{ minHeight: "100vh" }}
+        >
+          <CircularProgress />
+        </Grid>
+      ) : (
+        <Grid container spacing={3}>
+          {products.map((p: productCardProps) => (
+            <Grid key={p._id} size={{ xs: 12, sm: 6, md: 4 }}>
+              <ProductCard {...p}></ProductCard>
+            </Grid>
+          ))}
+        </Grid>
+      )}
     </Container>
   );
 };
